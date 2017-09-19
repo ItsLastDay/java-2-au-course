c935b1a5a13e716367e1b28d8d630ffe70ac8dce
Mikhail Koltsov <misha@koltsov.su>
2017-09-19 13:55:00 +0300

    Consider null value returining from supplier (could not solve for LockFreeLazy)

--- src/main/java/ru/spbau/lazy/LazyFactory.java
+++ src/main/java/ru/spbau/lazy/LazyFactory.java
@@ -7,11 +7,13 @@ public class LazyFactory {
     public static <T> Lazy<T> createSingleThreadLazy(Supplier<T> supplier) {
         return new Lazy<T>() {
             private T result = null;
+            private boolean wasComputed = false;
 
             @Override
             public T get() {
-                if (result == null) {
+                if (!wasComputed) {
                     result = supplier.get();
+                    wasComputed = true;
                 }
 
                 return result;
@@ -23,13 +25,15 @@ public class LazyFactory {
     public static <T> Lazy<T> createMultiThreadLazy(Supplier<T> supplier) {
         return new Lazy<T>() {
             private volatile T result = null;
+            private volatile boolean wasComputed = false;
 
             @Override
             public T get() {
-                if (result == null) {
+                if (!wasComputed) {
                     synchronized(this) {
-                        if (result == null) {
+                        if (!wasComputed) {
                             result = supplier.get();
+                            wasComputed = true;
                         }
                     }
                 }
